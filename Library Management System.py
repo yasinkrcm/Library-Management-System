7fee7a67d07ea3921464fee5965c86454227351a
Yasin karaçam <yasinkaracam67@gmail.com>
2024-02-14 18:08:20 +0300

    Update Library Management System.py

--- Library Management System.py
+++ Library Management System.py
@@ -1,6 +1,3 @@
-
-
-
 class Library:
     def listbooks(self):
         with open("books.txt","r",encoding="utf-8") as file:
@@ -19,7 +16,8 @@ class Library:
                 file.write(f"{title},{author},{releaseyear},{pages}\n")
             print("\nBook added successfully!")
 
-    def removebook(self, title):
+    def removebook(self,):
+        title = input("Enter the title of the book you want to remove: ")
         books = []
         with open("books.txt", "r", encoding="utf-8") as file:
             for book in file:
@@ -31,8 +29,6 @@ class Library:
                     file.write(f"{','.join(book)}\n")
         print(f"\nBook '{title}' removed successfully!")
 
-
-
 lib = Library()
 
 while True:
@@ -43,14 +39,14 @@ while True:
 3) Remove Book 
 4) Quit
 Enter your choice (1-4): """)
-    if choice == "4" :
-        break
+    
     if choice == "1" :
         lib.listbooks()
     elif choice == "2":
         lib.addbook()
     elif choice == "3":
-        title = input("Enter the title of the book you want to remove: ")
-        lib.removebook(title)    
+        lib.removebook()
+    elif choice == "4" :
+        break    
     else:
         print("Invalid choice. Please enter a number between 1 and 4.")
