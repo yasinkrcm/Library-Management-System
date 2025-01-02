b52184b0bae183bc28e9c5ed07c15ec9ac77dd07
Yasin karaçam <yasinkaracam67@gmail.com>
2025-01-02 21:37:30 +0300

    error management operations implemented

--- Library Management System.py
+++ Library Management System.py
@@ -1,33 +1,61 @@
 class Library:
-    def listbooks(self):
-        with open("books.txt","r",encoding="utf-8") as file:
+    def getBooks(self):
+        try:
+            books = []
+            file = open("books.txt","r+",encoding="utf-8")
             for book in file:
                 book = book.splitlines()[0].split(",")
-                books = []
                 books.append(book)
-                print(f"Book Title : {books[0][0]} , Book Author : {books[0][1]} ")
-                
-    def addbook(self):
+            file.close()
+            return books
+        except:
+            print("books.txt not exist")
+    
+    
+    def listBooks(self):
+        books = self.getBooks()
+        if books:
+            for book in books:
+                if len(book) >= 5:  
+                    print(f"{book[0]} - Book Title: {book[1]}, Book Author: {book[2]}, Pages: {book[4]}")
+                else:
+                    print("Incomplete book data.")
+        else:
+            print("No books in library.Please add book into books")
+            return 1
+
+    def addBook(self):
             title = input("Book title : ")
             author = input("Book author : ")
             releaseyear = input("First release year : ")
             pages = input("How many pages : ")
-            with open("books.txt","+a",encoding="utf-8") as file :
-                file.write(f"{title},{author},{releaseyear},{pages}\n")
+            id = 1
+            with open("books.txt","a+",encoding="utf-8") as file :
+                for book in file:
+                    id +=1 
+                    books = []
+                    book = book.splitlines()[0].split(",")
+                    books.append(book)
+                file.write(f"{int(id)},{title},{author},{releaseyear},{pages}\n")
             print("\nBook added successfully!")
 
-    def removebook(self,):
-        title = input("Enter the title of the book you want to remove: ")
-        books = []
-        with open("books.txt", "r", encoding="utf-8") as file:
-            for book in file:
-                book = book.strip().splitlines()[0].split(",")
-                books.append(book)
-        with open("books.txt", "w", encoding="utf-8") as file:
-            for book in books:
-                if book[0] != title:
-                    file.write(f"{','.join(book)}\n")
-        print(f"\nBook '{title}' removed successfully!")
+    def removeBook(self):
+        if self.listBooks():
+            return 1
+        else:
+            id = input("Enter the iD of the book you want to remove: ")
+            books = []
+            with open("books.txt", "r", encoding="utf-8") as file:
+                for book in file:
+                    book = book.strip().splitlines()[0].split(",")
+                    books.append(book)
+            with open("books.txt", "w", encoding="utf-8") as file:
+                for book in books:
+                    if book[0] != id:
+                        file.write(f"{','.join(book)}\n")
+                    else:
+                        print(f"\nBook '{book[1]}' removed successfully!")
+        
 
 lib = Library()
 
@@ -41,11 +69,11 @@ while True:
 Enter your choice (1-4): """)
     
     if choice == "1" :
-        lib.listbooks()
+        lib.listBooks()
     elif choice == "2":
-        lib.addbook()
+        lib.addBook()
     elif choice == "3":
-        lib.removebook()
+        lib.removeBook()
     elif choice == "4" :
         break    
     else:
